589bd72f00b484f4b4df22d36ecdb7bc6c572393
ethan022 <ethanyun022@gmail.com>
2025-03-27 13:57:53 +0900

    Update 0328.py

--- 0328.py
+++ 0328.py
@@ -6,19 +6,25 @@
 # 1. if 조건식:
 if True:
     print('참 입니다.')
+else:
+    print('거짓 입니다.')
 
 # 2. if 조건식:
 if False:
+    print('참 입니다.')
+else:
     print('거짓 입니다.')
 
 """ 조건문 예제 """
 age = 15
 
-if age > 16:
-    print('15살 이상입니다.')
+if age >= 20:
+    print('20살 이상입니다.')
+else:
+    print('미성년자입니다.')
 
 #  조건문과 상관없이 실행
-print('끝!')
+print(f'나이는 {age} 입니다.')
 
 # 3. if 조건식:
 
@@ -62,13 +68,75 @@ if 0:
     print("숫자가 0 입니다.")
 
 # 실습 1
-pw = input("비밀번호를 입력하세요: ")
+# pw = input("비밀번호를 입력하세요: ")
 
-if pw == "abc123":
-    print("비밀번호가 일치합니다.")
+# if pw == "abc123":
+#     print("비밀번호가 일치합니다.")
+# else:
+#     print('비밀번호가 틀렸습니다.')
 
-# 실습 2
-num = int(input("숫자를 입력하세요: "))
+# # 실습 2
+# num = int(input("숫자를 입력하세요: "))
 
-if num % 2 == 0:
-    print("짝수입니다.")
+# if num % 2 == 0:
+#     print("짝수입니다.")
+# else:
+#     print('홀수 입니다.')
+
+
+""" 조건문 예제 """
+age = int(input("입력해주세요"))
+
+if age >= 40:
+    print('40대 입니다.')
+elif age >= 30:
+    print('30대 입니다.')
+elif age >= 20:
+    print('20대 입니다.')
+else:
+    print('미성년자입니다.')
+
+""" 실습 3 """
+score = input("성적을 입력해주세요")
+
+if score.isdigit():
+    score = int(score)
+    if score >= 90:
+        print('학점: A')
+    elif score >= 80:
+        print('학점: B')
+    elif score >= 70:
+        print('학점: C')
+    elif score >= 60:
+        print('학점: D')
+    else:
+        print('학점: F')
+else:
+    print('숫자를 입력해주세요')
+
+
+age = int(input("나이를 입력해주세요"))
+# 결제 방식
+payment = input("결제 방식을 입력해주세요")
+
+if age >= 75:
+    print(f'{age}세 요금은 무료 입니다.')
+elif 20 <= age < 75:
+    if payment == '카드':
+        print(f'{age}세 카드 요금은 1200원 입니다.')
+    else:
+        print(f'{age}세 현금 요금은 1300원 입니다.')
+elif 14 <= age < 20:
+    if payment == '카드':
+        print(f'{age}세 카드 요금은 720원 입니다.')
+    else:
+        print(f'{age}세 현금 요금은 1000원 입니다.')
+elif 8 <= age < 14:
+    print(f'{age}세 요금은 450원 입니다.')
+else:
+    print(f'{age}세 요금은 무료 입니다.')
+
+
+print()
+print()
+print()
